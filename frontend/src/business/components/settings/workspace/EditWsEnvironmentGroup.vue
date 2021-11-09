d58f2d7e2c18cffe291124b016a449ac5e29f056
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-11-09 14:42:09 +0800

    test

--- frontend/src/business/components/settings/workspace/EditWsEnvironmentGroup.vue
+++ frontend/src/business/components/settings/workspace/EditWsEnvironmentGroup.vue
@@ -1,44 +1,33 @@
 <template>
   <div >
-    <el-form :inline="true" v-model="envGroupProject">
-        <el-row :gutter="2" >
-        <el-col :span="6" >
-          <el-form-item v-for="(item,index) in envGroupProject" :key="index" :value="item" >
-          <el-select  @change="handleProjectChange(envGroupProject[index].projectId)" v-model="envGroupProject[index].projectId" filterable clearable
-                     placeholder="请选择项目">
-            <el-option v-for="item in projectList" :key="item.id" :label="item.name" :value="item.id"></el-option>
-          </el-select>
-
-            <el-select  @change="ChangeEnvId" v-model="envGroupProject[index].name" filterable clearable
-                        placeholder="请选择环境">
-              <el-option v-for="item in environments" :key="item.id" :label="item.name" :value="item.id"></el-option>
+    <el-form v-loading="result.loading">
+      <el-form-item v-for="(item,index) in envGroupProject" :key="index">
+        <el-row type="flex" justify="space-between" :gutter="10">
+          <el-col :span="6">
+            <el-select v-model="item.projectId" filterable clearable style="width: 100%" @change="handleProjectChange(item)"
+                        placeholder="请选择项目">
+              <el-option v-for="(project, projectIndex) in projectList" :key="projectIndex" :label="project.name" :value="project.id"></el-option>
             </el-select>
-          </el-form-item>
-        </el-col>
+          </el-col>
 
-      <el-col :span="6">
-        <el-form-item v-for="(item,index) in envGroupProject" :key="index" :value="item">
-        <el-select  @change="ChangeEnvId" v-model="envGroupProject[index].name" filterable clearable
-                   placeholder="请选择环境">
-          <el-option v-for="item in environments" :key="item.id" :label="item.name" :value="item.id"></el-option>
-        </el-select>
-        </el-form-item>
-      </el-col>
-      <el-col :span="6">
-        <el-form-item v-for="(item,index) in envGroupProject" :key="index" :value="item" v-model="envGroupProject[index].config">
-            <span id='span' v-if="parseDomainName(envGroupProject[index].config)!='SHOW_INFO'">{{ parseDomainName(envGroupProject[index].config) }}</span>
-            <el-button size="mini" icon="el-icon-s-data" @click="showInfo(envGroupProject[index].config)" v-else>查看域名详情</el-button>
+          <el-col :span="6">
+            <el-select v-model="item.envId" filterable clearable style="width: 100%" @change="ChangeEnvId(item)"
+                        placeholder="请选择环境">
+              <el-option v-for="(environment, envIndex) in item.environments" :key="envIndex" :label="environment.name" :value="environment.id"></el-option>
+            </el-select>
+          </el-col>
 
-        </el-form-item>
-      </el-col>
-      <el-col :span="6">
-        <el-form-item v-for="(item,index) in envGroupProject" :key="index" :value="item">
-        <el-input  prop="description" show-overflow-tooltip placeholder="备注" >
-        </el-input>
-        </el-form-item>
-      </el-col>
+          <el-col :span="6">
+            <span v-if="item.domainName">{{ item.domainName }}</span>
+            <el-button size="mini" icon="el-icon-s-data" @click="showInfo(item.envId)" v-else>查看域名详情</el-button>
+          </el-col>
 
+          <el-col :span="6">
+            <el-input  prop="description" show-overflow-tooltip placeholder="备注" >
+            </el-input>
+          </el-col>
         </el-row>
+      </el-form-item>
     </el-form>
 
     <el-dialog title="域名列表" :visible.sync="domainVisible">
@@ -89,7 +78,7 @@
     },
     data() {
       return {
-        envGroupProject: Array,
+        envGroupProject: [],
         projectList: [],
         idNameMap: new Map(),
         currentProjectId: '',   //复制、创建、编辑环境时所选择项目的id
@@ -104,34 +93,27 @@
         envNameList: [],
         currentEnvironment: new Environment(),
         envProjectId: '',
-        environments:[]
+        environments:[],
+        result: {}
       }
     },
     created() {
-      this.getEnvironment();
       this.list();
+      this.getEnvironment();
     },
 
     activated() {
       this.list();
     },
 
-    watch: {
-      //当创建及复制环境所选择的项目变化时，改变当前环境对应的projectId
-      currentProjectId() {
-        // el-select什么都不选时值为''，为''的话也会被当成有效的projectId传给后端，转化使其无效
-        if (this.currentProjectId === '') {
-          this.currentEnvironment.projectId = null;
-        } else {
-          this.currentEnvironment.projectId = this.currentProjectId;
-        }
-      },
-    },
-
-
     methods: {
-      showInfo(row) {
-        const config = JSON.parse(row);
+      showInfo(id) {
+        const index = this.environments.findIndex(e => e.id === id);
+        if (index === -1) {
+          return "";
+        }
+        let environment = this.environments[index]
+        const config = JSON.parse(environment.config);
         this.conditions = config.httpConfig.conditions;
         this.domainVisible = true;
       },
@@ -203,62 +185,136 @@
           this.total = response.data.itemCount;
         })
       },
-      handleProjectChange(projectId) {   //项目选择下拉框选择其他项目后清空“启用条件”,因为项目变了模块也就变了。
-        this.currentEnvironment.config.httpConfig.conditions = [];
-        if(projectId) {
-          this.envProjectId = projectId;
-          this.getEnvNameList(projectId);
+      handleProjectChange(item) {   //项目选择下拉框选择其他项目后清空“启用条件”,因为项目变了模块也就变了。
+        if (item && item.projectId) {
+          this.$get('/api/environment/list/' + item.projectId, res => {
+            this.$set(item, 'environments', res.data);
+          });
+        } else {
+          this.$set(item, 'environments', []);
         }
+        this.$set(item, 'envId', "");
+        this.$set(item, 'domainName', "-");
       },
-      ChangeEnvId(envId){
-        if(envId) {
-          this.getConfig(envId);
+      ChangeEnvId(item){
+        if (!item.projectId) {
+          this.$warning("请先选择项目！");
+          return;
         }
-      },
-      getConfig(envId){
-        console.log(envId)
-        this.result = this.$get("/api/environment/get/" + envId , response => {
-          this.envGroupProject.config = response.data.config
-          let env = this.envGroupProject.config;
-          this.parseDomainName(env);
-          // this.showInfo(env);
-        })
-
-      },
-      getEnvironment(envGroupId) {
-        envGroupId = this.envGroupId
-        let url = '/api/environment/list/envGroupProject/' + envGroupId;
-        this.result = this.$get(url, response => {
-          this.envGroupProject = response.data
-        })
-      },
-      parseDomainName(environment) {   //解析出环境域名用于前端展示
-        console.log(environment)
+        let environments = item.environments;
+        let index = environments.findIndex(e => e.id === item.envId);
+        if (index === -1) {
+          this.$set(item, "domainName", '-');
+          return ;
+        }
+        let environment = environments[index].config;
         if (environment) {
           const config = JSON.parse(environment);
           if (config.httpConfig && !config.httpConfig.conditions) {
             if (config.httpConfig.protocol && config.httpConfig.domain) {
-              return config.httpConfig.protocol + "://" + config.httpConfig.domain;
+              let domain = config.httpConfig.protocol + "://" + config.httpConfig.domain;
+              this.$set(item, "domainName", domain);
+              return ;
             }
-            return "";
           } else {
             if (config.httpConfig.conditions.length === 1) {
               let obj = config.httpConfig.conditions[0];
               if (obj.protocol && obj.domain) {
-                return obj.protocol + "://" + obj.domain;
+                this.$set(item, "domainName", obj.protocol + "://" + obj.domain);
+                return;
               }
-            } else if (config.httpConfig.conditions.length > 1) {
-              return "SHOW_INFO";
-            } else {
-              return "";
             }
           }
         } else {  //旧版本没有对应的config数据,其域名保存在protocol和domain中
-          return environment.protocol + '://' + environment.domain;
+          this.$set(item, "domainName", environment.protocol + '://' + environment.domain);
+          return ;
         }
-      }
-    },
+        this.$set(item, "domainName", undefined);
 
+      },
+      // getConfig(envId){
+      //   console.log(envId)
+      //   this.result = this.$get("/api/environment/get/" + envId , response => {
+      //     this.envGroupProject.config = response.data.config
+      //     let env = this.envGroupProject.config;
+      //     this.parseDomainName(env);
+      //     // this.showInfo(env);
+      //   })
+      //
+      // },
+      getEnvironment(envGroupId) {
+        envGroupId = this.envGroupId
+        let url = '/api/environment/list/envGroupProject/' + envGroupId;
+        this.result = this.$get(url, response => {
+          this.envGroupProject = response.data;
+          // 初始化环境数据
+          this.envGroupProject.forEach(env => {
+            let {projectId} = env;
+            this.$get('/api/environment/list/' + projectId, res => {
+              this.$set(env, 'environments', res.data);
+            });
+            this._parseDomainName(env);
+          })
+        })
+      },
+      // parseDomainName(item) {   //解析出环境域名用于前端展示
+      //   if (!item.envId || !item.environments) {
+      //     return "";
+      //   }
+      //   const index = item.environments.findIndex(e => e.id === item.envId);
+      //   if (index === -1) {
+      //     return "";
+      //   }
+      //   let environment = item.environments[index].config;
+      //   if (environment) {
+      //     const config = JSON.parse(environment);
+      //     if (config.httpConfig && !config.httpConfig.conditions) {
+      //       if (config.httpConfig.protocol && config.httpConfig.domain) {
+      //         return config.httpConfig.protocol + "://" + config.httpConfig.domain;
+      //       }
+      //       return "";
+      //     } else {
+      //       if (config.httpConfig.conditions.length === 1) {
+      //         let obj = config.httpConfig.conditions[0];
+      //         if (obj.protocol && obj.domain) {
+      //           return obj.protocol + "://" + obj.domain;
+      //         }
+      //       } else if (config.httpConfig.conditions.length > 1) {
+      //         return "SHOW_INFO";
+      //       } else {
+      //         return "";
+      //       }
+      //     }
+      //   } else {  //旧版本没有对应的config数据,其域名保存在protocol和domain中
+      //     return environment.protocol + '://' + environment.domain;
+      //   }
+      // },
+      _parseDomainName(item) {   //解析出环境域名用于前端展示
+        if (!item) {
+          this.$set(item, "domainName", "-");
+          return;
+        }
+        let environment = item.config;
+        if (environment) {
+          const config = JSON.parse(environment);
+          if (config.httpConfig && !config.httpConfig.conditions) {
+            if (config.httpConfig.protocol && config.httpConfig.domain) {
+              let domain = config.httpConfig.protocol + "://" + config.httpConfig.domain;
+              this.$set(item, "domainName", domain);
+            }
+          } else {
+            if (config.httpConfig.conditions.length === 1) {
+              let obj = config.httpConfig.conditions[0];
+              if (obj.protocol && obj.domain) {
+                this.$set(item, "domainName", obj.protocol + "://" + obj.domain);
+              }
+            }
+          }
+        } else {  //旧版本没有对应的config数据,其域名保存在protocol和domain中
+          this.$set(item, "domainName", environment.protocol + '://' + environment.domain);
+        }
+      },
+    },
 
   }
 </script>
